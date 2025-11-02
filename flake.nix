18c65669361fabe3a82b0567935e2c793863f829
Kevin Rao <kevinrao2000@gmail.com>
2025-11-02 17:09:20 -0500

    Formatted flake.nix

--- flake.nix
+++ flake.nix
@@ -5,65 +5,59 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-      
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs {
-            system = "${system}";
-            config = {
-              allowUnfree = true;
-              cudaSupport = true;
-            };
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {
+          system = "${system}";
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
           };
-          nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
-        in
-          {
-            formatter."${system}" = nixpkgs.legacyPackages.${system}.alejandra;
-            default = pkgs.mkShell {
-              packages = with pkgs; [
-                python3
-                uv
-                ffmpeg
-                fmt.dev
-                cudaPackages.cuda_cudart
-                cudatoolkit
-                cudaPackages.cudnn
-                libGLU
-                libGL
-                xorg.libXi
-                xorg.libXmu
-                freeglut
-                xorg.libXext
-                xorg.libX11
-                xorg.libXv
-                xorg.libXrandr
-                zlib
-                ncurses
-                stdenv.cc
-                binutils
-              ];
+        };
+        nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
+      in {
+        formatter."${system}" = nixpkgs.legacyPackages.${system}.alejandra;
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            uv
+            ffmpeg
+            fmt.dev
+            cudaPackages.cuda_cudart
+            cudatoolkit
+            cudaPackages.cudnn
+            libGLU
+            libGL
+            xorg.libXi
+            xorg.libXmu
+            freeglut
+            xorg.libXext
+            xorg.libX11
+            xorg.libXv
+            xorg.libXrandr
+            zlib
+            ncurses
+            stdenv.cc
+            binutils
+          ];
 
-              shellHook = ''
-                unset PYTHONPATH
-              export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
-        export CUDA_PATH=${pkgs.cudatoolkit}
-        export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
-        export EXTRA_CCFLAGS="-I/usr/include"
-        export CMAKE_PREFIX_PATH="${pkgs.fmt.dev}:$CMAKE_PREFIX_PATH"
-        export PKG_CONFIG_PATH="${pkgs.fmt.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-              uv sync
-              . .venv/bin/activate
-              '';
-            };
-          }
-      );
-    };
+          shellHook = ''
+                    unset PYTHONPATH
+                  export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
+            export CUDA_PATH=${pkgs.cudatoolkit}
+            export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
+            export EXTRA_CCFLAGS="-I/usr/include"
+            export CMAKE_PREFIX_PATH="${pkgs.fmt.dev}:$CMAKE_PREFIX_PATH"
+            export PKG_CONFIG_PATH="${pkgs.fmt.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+                  uv sync
+                  . .venv/bin/activate
+          '';
+        };
+      }
+    );
+  };
 }
