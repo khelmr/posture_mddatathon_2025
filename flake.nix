22ff9747b2e4c41afe9e82da72a5a4fe84b038cd
Kevin Rao <kevinrao2000@gmail.com>
2025-11-02 17:08:42 -0500

    Added dev tools. Attempted to enable cuda in flake

--- flake.nix
+++ flake.nix
@@ -10,27 +10,60 @@
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+      
     in
     {
       devShells = forAllSystems (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            system = "${system}";
+            config = {
+              allowUnfree = true;
+              cudaSupport = true;
+            };
+          };
+          nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
         in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.python3
-              pkgs.uv
-            ];
+          {
+            formatter."${system}" = nixpkgs.legacyPackages.${system}.alejandra;
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                python3
+                uv
+                ffmpeg
+                fmt.dev
+                cudaPackages.cuda_cudart
+                cudatoolkit
+                cudaPackages.cudnn
+                libGLU
+                libGL
+                xorg.libXi
+                xorg.libXmu
+                freeglut
+                xorg.libXext
+                xorg.libX11
+                xorg.libXv
+                xorg.libXrandr
+                zlib
+                ncurses
+                stdenv.cc
+                binutils
+              ];
 
-            shellHook = ''
-              unset PYTHONPATH
+              shellHook = ''
+                unset PYTHONPATH
+              export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
+        export CUDA_PATH=${pkgs.cudatoolkit}
+        export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
+        export EXTRA_CCFLAGS="-I/usr/include"
+        export CMAKE_PREFIX_PATH="${pkgs.fmt.dev}:$CMAKE_PREFIX_PATH"
+        export PKG_CONFIG_PATH="${pkgs.fmt.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
               uv sync
               . .venv/bin/activate
-            '';
-          };
-        }
+              '';
+            };
+          }
       );
     };
 }
