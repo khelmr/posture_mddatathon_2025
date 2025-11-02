303ecfeedd6ed24d1a83fd30ed85cbdb16e12f12
Kevin Rao <kevinrao2000@gmail.com>
2025-11-02 18:41:27 -0500

    Flake installs CUDA packages and sets variables only when USE_CUDA is set.

--- flake.nix
+++ flake.nix
@@ -18,43 +18,55 @@
             cudaSupport = true;
           };
         };
+        # Read from Nix configuration or environment variable
+        useCuda =
+          builtins.getEnv "USE_CUDA"
+          == "1"
+          || (pkgs.config.useCuda or false);
         nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
       in {
         formatter."${system}" = nixpkgs.legacyPackages.${system}.alejandra;
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-            uv
-            ffmpeg
-            fmt.dev
-            cudaPackages.cuda_cudart
-            cudatoolkit
-            cudaPackages.cudnn
-            libGLU
-            libGL
-            xorg.libXi
-            xorg.libXmu
-            freeglut
-            xorg.libXext
-            xorg.libX11
-            xorg.libXv
-            xorg.libXrandr
-            zlib
-            ncurses
-            stdenv.cc
-            binutils
-          ];
+          packages = with pkgs;
+            [
+              python3
+              uv
+            ]
+            ++ lib.optionals useCuda [
+              # cuda packages
+              ffmpeg
+              fmt.dev
+              cudaPackages.cuda_cudart
+              cudatoolkit
+              cudaPackages.cudnn
+              libGLU
+              libGL
+              xorg.libXi
+              xorg.libXmu
+              freeglut
+              xorg.libXext
+              xorg.libX11
+              xorg.libXv
+              xorg.libXrandr
+              zlib
+              ncurses
+              stdenv.cc
+              binutils
+            ];
 
           shellHook = ''
-                    unset PYTHONPATH
-                  export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
-            export CUDA_PATH=${pkgs.cudatoolkit}
-            export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
-            export EXTRA_CCFLAGS="-I/usr/include"
-            export CMAKE_PREFIX_PATH="${pkgs.fmt.dev}:$CMAKE_PREFIX_PATH"
-            export PKG_CONFIG_PATH="${pkgs.fmt.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-                  uv sync
-                  . .venv/bin/activate
+              unset PYTHONPATH
+              ${lib.optionalString useCuda ''
+                      export LD_LIBRARY_PATH="${nvidiaPackage}/lib:$LD_LIBRARY_PATH"
+                export CUDA_PATH=${pkgs.cudatoolkit}
+                export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
+                              export EXTRA_CCFLAGS="-I/usr/include"
+              export CMAKE_PREFIX_PATH="${pkgs.fmt.dev}:$CMAKE_PREFIX_PATH"
+              export PKG_CONFIG_PATH="${pkgs.fmt.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+            ''}
+            uv sync
+            . .venv/bin/activate
           '';
         };
       }
